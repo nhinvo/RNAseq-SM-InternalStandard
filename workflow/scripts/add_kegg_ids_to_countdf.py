ba6b42b4584d26ad8223a313b608b3765586a2a8
Konnor von Emster <konnorvonemster@gmail.com>
2022-01-31 12:11:38 -0500

    added post-processing scripts

---
+++ workflow/scripts/add_kegg_ids_to_countdf.py
@@ -0,0 +1,70 @@
+from pathlib import Path
+import pandas as pd
+import logging as log
+import urllib.error
+import yaml
+
+import Bio.KEGG.REST as kegg_rest
+
+log.basicConfig(format='%(levelname)s:%(message)s', level=log.DEBUG)
+
+
+with open('../../config/config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+results_dir = Path(config["results"])
+
+# results_dir = Path(snakemake.config["results"])
+
+counts_df = pd.read_csv(results_dir / "counts.tsv", sep='\t', index_col=[0,1,2], header=[0,1])
+
+log.debug(f"counts_df:\n{counts_df}\n\n")
+
+kegg_pathway_columns = {'BRITE':'brite', 'KEGG_Module':'module', 'KEGG_Pathway':'pathway', 'KEGG_Reaction':'reaction', 'KEGG_rclass':'rclass'}
+
+def ident_kegg_term(db, id):
+    try: 
+        return kegg_rest.kegg_find(db, id).readline().split('\n')[0].split('\t')[1]
+    except IndexError as e:
+        log.error(f'problematic term:\n{kegg_rest.kegg_find(db, id).readline()}')
+        return ""
+    except urllib.error.URLError as e:
+        return e.read().decode("utf8", 'ignore')
+
+dfs = []
+for col, db in kegg_pathway_columns.items():
+    log.info(f"{col}\t{db}")
+    unique_terms = set()
+    if col in counts_df['annotation_data'].columns:
+        for row in counts_df['annotation_data'][col].to_list():
+            unique_terms |= set(str(row).split(","))
+        unique_terms -= {'-', 'nan'}
+        term_table = []
+        for i in unique_terms:
+            term = ident_kegg_term(db, i)
+            log.info(f"{col}\t{i}\t{term}")
+            term_table.append([col, db, i, term])
+        dfs.append(pd.DataFrame(term_table, columns=['ref_col', 'kegg_df_ref', 'kegg_id', 'kegg_term']))
+
+ref_table = pd.concat(dfs, axis=0)
+
+counts_df_kegg_cols = ref_table.ref_col.unique()
+
+counts_df['annotation_data'] = counts_df['annotation_data'].fillna(value={col:"" for col in counts_df_kegg_cols})
+counts_df['annotation_data'] = counts_df['annotation_data'].replace(to_replace="-", value={col:"" for col in counts_df_kegg_cols})
+
+for col in ref_table.ref_col.unique():
+    ref_table_subset = ref_table[ref_table['ref_col']==col]
+    ref_dict = {row.loc['kegg_id']:row.loc['kegg_term'] for i, row in ref_table_subset.iterrows()}
+    # log.debug(f"ref_dict:\n{ref_dict}")
+    col_ids = counts_df['annotation_data'][col].to_list()
+    col_terms = []
+    for row in col_ids:
+        if row == "":
+            row_label = ""
+        else:
+            row_label = ", ".join([str(ref_dict[i]) for i in str(row).split(',')])
+        col_terms.append(row_label)
+
+    counts_df.loc[:,('annotation_data',f"{col}_terms")] = col_terms
+
+counts_df.to_csv(results_dir / "counts.tsv", sep='\t')
