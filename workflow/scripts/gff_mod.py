fb940259cd308731ace1bb3adba72feac0553dcd
Konnor von Emster <konnorvonemster@gmail.com>
2022-02-11 16:14:33 -0500

    potential fix to read mapping ambiguity issue

--- workflow/scripts/gff_mod.py
+++ workflow/scripts/gff_mod.py
@@ -4,7 +4,12 @@ import gffpandas.gffpandas as gffpd
 print(snakemake.input)
 print(snakemake.output)
 
+feature_types_to_keep = snakemake.config["feature_types_to_keep"]
+
 annotation = gffpd.read_gff3(str(snakemake.input))
+
+#remove non-desired feature types
+annotation.df = annotation.df[annotation.df.type.isin(feature_types_to_keep)]
 
 #create just one gff with feature replaced as exon purely for htseq counting. Then use the unmodifed gff in the post-htseq-parsing.py
 annotation.df.iloc[:,2] = 'exon'
