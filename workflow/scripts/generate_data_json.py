72a0e11c661f96510700b0e4afbd3fa6bdafe42f
Konnor von Emster <konnor@vonemster.com>
2022-03-01 20:22:21 -0500

    small fixes to json creation script

--- workflow/scripts/generate_data_json.py
+++ workflow/scripts/generate_data_json.py
@@ -8,7 +8,7 @@ log.basicConfig(format='%(levelname)s:%(message)s', level=log.INFO)
 
 counts_df = pd.read_csv(snakemake.input['counts'], sep='\t', header=[0,1], index_col=[0,1,2])
 metadata_df = pd.read_csv(snakemake.input['metadata'], sep='\t', header=0, index_col=0)
-samples_df = pd.read_csv(snakemake.input['samples'], sep='\t', header=0, index_col='sample_name')
+samples_df = pd.read_csv(snakemake.input['samples'], sep='\t', header=0)
 ref_table = pd.read_csv(snakemake.input['ref_table'], sep='\t', index_col=['ref_col','id'])
 
 log.debug(f"counts_df:\n{counts_df}\n\n")
@@ -21,9 +21,9 @@ all_data = {}
 with open(snakemake.input['config']) as file:
     all_data['config'] = yaml.load(file, Loader=yaml.FullLoader)
 
-all_data['samples'] = samples_df.to_dict(orient='index')
+all_data['samples'] = samples_df.to_dict(orient='records')
 
-all_data['metadata'] = metadata_df.to_dict(orient='index')
+all_data['metadata'] = metadata_df.to_dict(orient='dict')
 
 log.debug(f"ref_table.index.unique(level='ref_col'):\n{ref_table.index.unique(level='ref_col')}\n\n")
 
@@ -37,8 +37,8 @@ counts_df = counts_df.reset_index(level='type', col_level=1, col_fill='annotatio
 all_data['annotation_data'] = {}
 all_data['count_data'] = {}
 for organism in counts_df.index.unique(level='organism'):
-    all_data['annotation_data'][organism] = counts_df.loc[organism]['sample_data'].to_dict(orient='index')
-    all_data['count_data'][organism] = counts_df.loc[organism]['annotation_data'].to_dict(orient='index')
+    all_data['count_data'][organism] = counts_df.loc[organism]['sample_data'].to_dict(orient='dict')
+    all_data['annotation_data'][organism] = counts_df.loc[organism]['annotation_data'].to_dict(orient='dict')
 
 with open(snakemake.output['data_json'], "w") as outfile:
     json.dump(all_data, outfile)
